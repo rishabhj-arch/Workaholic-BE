38c65933b39e3df66790914cf63acdfa13f17907
rishabhj-arch <rishabhj@igeektech.in>
2025-11-07 16:25:57 +0530

    initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { registerUser, loginUser, updateUserDetails, getUserByEmail, deleteUserDetails } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/:email', getUserByEmail)
+router.put('/update', protect, updateUserDetails)
+router.delete('/details', protect, deleteUserDetails)
+export default router
